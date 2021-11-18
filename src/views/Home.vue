d2108756b5a6e812b847e509b0a11eb80bd44cac
Palanka777 <dpont@iesebre.com>
2021-11-18 19:28:41 +0100

    Interficie grafica de comptador feta a falta de personalitzar

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,28 +3,49 @@
     <ion-header :translucent="true">
       <ion-toolbar>
         <ion-title>Time Fighter</ion-title>
+        <ion-buttons slot="primary">
+          <ion-button color="primary" fill="solid" @click="infoAlert">
+            <ion-icon :icon="infoIcon"></ion-icon>
+          </ion-button>
+        </ion-buttons>
       </ion-toolbar>
     </ion-header>
     
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
+      <ion-header class="ion-no-border ion-padding-top ion-padding-horizontal">
+        <ion-grid>
+          <ion-row>
+            <ion-col class="ion-text-start">
+              Your Score: {{ score }}
+            </ion-col>
+            <ion-col class="ion-text-end">
+              Time Left: {{ timeLeft }}
+            </ion-col>
+          </ion-row>
+        </ion-grid>
       </ion-header>
     
       <div id="container">
-        <strong>Yes: TODO afegit el boto de comptador</strong>
-        <p>Hola Trempat! <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+        <ion-button color="primary" @click="tapMe">TAP ME</ion-button>
       </div>
     </ion-content>
   </ion-page>
 </template>
 
 <script>
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import {
+  alertController,
+  IonButton,
+  IonButtons, IonCol,
+  IonContent, IonGrid,
+  IonHeader,
+  IonIcon,
+  IonPage, IonRow,
+  IonTitle,
+  IonToolbar, toastController
+} from '@ionic/vue';
 import { defineComponent } from 'vue';
-
+import {informationCircleOutline} from "ionicons/icons"
 export default defineComponent({
   name: 'Home',
   components: {
@@ -32,7 +53,46 @@ export default defineComponent({
     IonHeader,
     IonPage,
     IonTitle,
-    IonToolbar
+    IonToolbar,
+    IonButtons,
+    IonButton,
+    IonIcon,
+    IonGrid,
+    IonCol,
+    IonRow
+  },
+  setup(){
+    return{
+      infoIcon: informationCircleOutline,
+      score: 0,
+      timeLeft: 60
+    }
+  },
+  methods: {
+    async infoAlert() {
+      const alert = await alertController
+          .create({
+            header: 'Time Fighter 1.1',
+            subHeader: 'Creat per Palanka',
+            message: 'Podeu trobar el codi font al meu <a href="https://github.com/Palanka777/ComptadorIonic.git">repositori del github</a>',
+            buttons: ['OK'],
+          });
+      await alert.present();
+    },
+     async tapMe(){
+      console.log('TODO TAP ME');
+
+      //TOAST
+      const toast = await toastController.create({
+        color: 'dark',
+        duration: 2000,
+        message: 'Paired successfully',
+        showCloseButton: true
+      });
+
+      await toast.present();
+
+    }
   }
 });
 </script>
